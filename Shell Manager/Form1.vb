69a791306acdafce5197e7298802806e5f4f60d6
andrew98450 <ktpss96034@yahoo.com.tw>
2016-04-18 21:12:38 +0800

    Shell Manager Code with Server

---
+++ Shell Manager/Form1.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Net.Sockets
+Imports System.Text
+Public Class Form1
+    Dim tcp As New TcpClient
+    Dim ns As NetworkStream
+    Public Sub comand()
+        Try
+            Dim rbyte As Byte() = New Byte(1000) {}
+            ns.Read(rbyte, 0, rbyte.Length)
+            ns.Flush()
+            Dim w As String = Encoding.ASCII.GetString(rbyte)
+            Dim tamanho As Integer = Convert.ToInt16(w)
+            Dim r As Byte() = New Byte(tamanho) {}
+            ns.Read(r, 0, r.Length)
+            ns.Flush()
+            Dim show As String = Encoding.ASCII.GetString(r)
+            RichTextBox1.Text = show
+            ns.Flush()
+        Catch ex As Exception
+        End Try
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            tcp.Connect(TextBox1.Text, TextBox2.Text)
+            ns = tcp.GetStream
+            MessageBox.Show("Connect Successfly...")
+        Catch ex As Exception
+            MessageBox.Show("Connect Fali...")
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            If tcp.Connected = True Then
+                Dim bytes As Byte() = Encoding.ASCII.GetBytes(TextBox3.Text)
+                ns.Write(bytes, 0, bytes.Length)
+                ns.Flush()
+                comand()
+            End If
+        Catch ex As Exception
+        End Try
+    End Sub
+End Class
---
+++ server/Module1.vb
@@ -0,0 +1,60 @@
+﻿Imports System.Net.Sockets
+Imports System.Text
+Imports System.IO
+Imports System.Runtime.InteropServices
+
+Module Module1
+    Dim list As TcpListener
+    Dim tcp As New TcpClient
+    Dim ns As NetworkStream
+    Public Sub re()
+        While True
+            Try
+                Dim bytes = New Byte(1000) {}
+                ns.Read(bytes, 0, bytes.Length)
+                ns.Flush()
+                Dim msg As String = Encoding.ASCII.GetString(bytes)
+                ev(msg)
+            Catch ex As Exception
+                End
+            End Try
+        End While
+    End Sub
+    Public Sub ev(ByVal comando As String)
+        Try
+            Console.WriteLine(comando)
+            Dim start As ProcessStartInfo = New ProcessStartInfo()
+            start.FileName = "cmd.exe"
+            start.Arguments = "/C " + comando
+            start.UseShellExecute = False
+            start.RedirectStandardOutput = True
+            Using pro As Process = Process.Start(start)
+                Using read As StreamReader = pro.StandardOutput
+                    Dim rsule As String = read.ReadToEnd
+                    Dim ta As Integer = rsule.Length
+                    Dim t As String = Convert.ToString(ta)
+                    Dim bytes As Byte() = Encoding.ASCII.GetBytes(t)
+                    ns.Write(bytes, 0, bytes.Length)
+                    ns.Flush()
+                    Dim com As Byte() = Encoding.ASCII.GetBytes(rsule)
+                    ns.Write(com, 0, com.Length)
+                    ns.Flush()
+                End Using
+            End Using
+        Catch ex As Exception
+            End
+        End Try
+    End Sub
+    Sub Main()
+        Try
+            list = New TcpListener(2000)
+            list.Start()
+            tcp = list.AcceptTcpClient
+            ns = tcp.GetStream
+            re()
+        Catch ex As Exception
+            End
+        End Try
+    End Sub
+
+End Module
